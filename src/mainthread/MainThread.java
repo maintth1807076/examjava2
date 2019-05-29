49221579141134b613ec2ae80626f67d1b36145f
maintth1807076 <maintth1807076@fpt.edu.vn>
2019-05-29 16:48:48 +0700

    repair

--- src/mainthread/MainThread.java
+++ src/mainthread/MainThread.java
@@ -2,11 +2,13 @@ package mainthread;
 
 import utility.Menu;
 
-import java.io.IOException;
-
 public class MainThread {
-    public static void main(String[] args) throws IOException {
-        Menu menu = new Menu();
-        menu.createMenu();
+    public static void main(String[] args) {
+        try{
+            Menu menu = new Menu();
+            menu.createMenu();
+        } catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }
     }
 }
